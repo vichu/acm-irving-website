fd092e1f87327a2bead76ea30d4e47bb5c89b2e7
Vish Ranganathan <vranganathan@yandex.com>
2026-03-03 18:42:08 -0800

    chore: remove Resources nav link

--- app/public/wp-content/themes/acm-irving/header.php
+++ app/public/wp-content/themes/acm-irving/header.php
@@ -58,7 +58,6 @@ function acm_irving_fallback_menu() {
     echo '<li><a href="' . esc_url(home_url('/about')) . '">About</a></li>';
     echo '<li><a href="' . esc_url(home_url('/events')) . '">Events</a></li>';
     echo '<li><a href="' . esc_url(home_url('/officers')) . '">Officers</a></li>';
-    echo '<li><a href="' . esc_url(home_url('/resources')) . '">Resources</a></li>';
     echo '<li><a href="' . esc_url(home_url('/contact')) . '">Contact</a></li>';
     echo '<li><a href="https://www.acm.org/membership" target="_blank" class="nav-cta">Join ACM</a></li>';
     echo '</ul>';
